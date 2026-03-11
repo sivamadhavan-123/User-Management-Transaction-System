70ff5c85ac1f13b7bb32b132565cc228e0ef81df
Siva M <sivapaivalasa@gmail.com>
2026-03-11 11:33:37 +0530

    check

--- src/main/java/org/example/Filters/SigninFilter.java
+++ src/main/java/org/example/Filters/SigninFilter.java
@@ -23,12 +23,12 @@ public class SigninFilter implements Filter {
 
         HttpServletRequest req = (HttpServletRequest) servletRequest;
         HttpServletResponse resp = (HttpServletResponse) servletResponse;
-
-        String username = req.getParameter("username");
-        String password = req.getParameter("password");
-
-        final String regex_username = "^[A-Za-z0-9]{3,50}$";
-        final String regex_password = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@!$*%?&]).{8,}$";
+//
+//        String username = req.getParameter("username");
+//        String password = req.getParameter("password");
+//
+//        final String regex_username = "^[A-Za-z0-9]{3,50}$";
+//        final String regex_password = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@!$*%?&]).{8,}$";
 
 
         HttpSession session = req.getSession(false);
@@ -36,20 +36,20 @@ public class SigninFilter implements Filter {
         if (session != null) {
             out.println("logout previous signin");
             logger.info("logout previous signin");
-            return;
-        } else {
-
-            if (username == null || !username.matches(regex_username)) {
-                out.println("Invalid username");
-                logger.warn("Invalid username");
-                return;
-            }
-            if (password == null || !password.matches(regex_password)) {
-                out.println("Invalid password");
-                logger.warn("Invalid password");
-                return;
-            }
-        }
+            return;}
+//        } else {
+//
+//            if (username == null || !username.matches(regex_username)) {
+//                out.println("Invalid username");
+//                logger.warn("Invalid username");
+//                return;
+//            }
+//            if (password == null || !password.matches(regex_password)) {
+//                out.println("Invalid password");
+//                logger.warn("Invalid password");
+//                return;
+//            }
+//        }
         filterChain.doFilter(servletRequest, servletResponse);
         logger.trace("Exiting SigninFilter.doFilter");
 
